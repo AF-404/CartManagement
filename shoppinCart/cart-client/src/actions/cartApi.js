85428e3517a70ff3b62b3c58745bf1d16768833b
udara-pk <53249224+udara-pk@users.noreply.github.com>
2020-05-24 21:13:38 +0530

    Update cartApi.js

--- shoppinCart/cart-client/src/actions/cartApi.js
+++ shoppinCart/cart-client/src/actions/cartApi.js
@@ -7,7 +7,6 @@ export default {
     cart(url = baseUrl + 'cart/') {
         return {
             fetchAll: () => axios.get(url),
-            fetchById: id => axios.get(url + id),
             create: newRecord => axios.post(url, newRecord),
             update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
             delete: id => axios.delete(url + id)
